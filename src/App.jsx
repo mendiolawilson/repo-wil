7dce6a0d4602d3a921a2f7952bfef74a11736e6c
mendiola wilson <mendiolawilson@yahoo.com>
2022-10-29 02:02:10 +0800

    4 commit

--- src/App.jsx
+++ src/App.jsx
@@ -28,21 +28,7 @@ export default function App() {
             } else {
                 ref.current.pause();
             }
-        });
-
-    // function AudioPlayer({ src, isPlaying, duration }) {
-    //     const ref = useRef(null);
-
-    //     useEffect(() => {
-    //         if (isPlaying) {
-    //             ref.current.play();
-    //         } else {
-    //             ref.current.pause();
-    //         }
-    //     });
-       
-    //     return <audio ref={ref} src={src} id="audios" onEnded={handleVideoEnded} />;
-    // }
+        }); 
     
     const handleVideoEnded = () => {
         setIsPlaying(!isPlaying);
@@ -64,14 +50,14 @@ export default function App() {
                     <Header />
                     <div className="container mx-auto my-[60px]">
                         <div className="grid grid-cols-1 sm:grid-cols-2 content-center">
-                            <div className="relative m-auto"> <img className="" src="../src/assets/left_picture.png" alt="" />
+                            <div className="relative m-auto"> <img className="" src="./assets/left_picture.png" alt="" />
                                 <div className="absolute bottom-[60px] right-[100px] bg-[#999999db] border-[#ffffff8a] rounded-full py-[0px] pl-[10px] pr-[25px]">
                                     <div className="flex items-center md:order-2">
                                         <span className="m-auto inline-flex justify-center items-center p-2 text-sm text-gray-500 rounded cursor-pointer ">
                                             <div className="relative">
-                                                <img className="w-11 h-11 rounded-full" src="../src/assets/profile.png" alt="" />
+                                                <img className="w-11 h-11 rounded-full" src="./assets/profile.png" alt="" />
                                                 <button className="w-11 h-11 rounded-full absolute top-0 bg-[#000000a6]" onClick={() => played(current_song, current_artist, current_src)}>
-                                                    {isPlaying ? <img className="rounded-full p-[10px]" src="../src/assets/pause.png" alt="" /> : <img className="rounded-full p-[10px]" src="../src/assets/play.png" alt="" />}
+                                                    {isPlaying ? <img className="rounded-full p-[10px]" src="./assets/pause.png" alt="" /> : <img className="rounded-full p-[10px]" src="./assets/play.png" alt="" />}
                                                 </button>
                                             </div>
                                         </span>
@@ -128,12 +114,12 @@ export default function App() {
                                 {playlists.map(function (item, i) {
                                     return <div className="p-[30px] m-auto">
                                         <div className="bg-[#390066] rounded-[25px]">
-                                            <div className={"rounded-[25px] bg-blend-overlay bg-[#ffffffba] bg-cover bg-[url('./assets/songs_images/Call me_Cover (front)_e.jpg')]"} style={{
+                                            <div className={"rounded-[25px] bg-blend-overlay bg-[#ffffffba] bg-cover bg-[url('./songs_images/Call me_Cover (front)_e.jpg')]"} style={{
                                                 backgroundImage: 'url("./songs_images/' + item.img_src + '")'
                                             }}>
                                                 <div className="flex items-center order-2 px-[20px] py-[5px]">
                                                     <span data-dropdown-toggle="language-dropdown-menu " className="mr-[20px] inline-flex justify-center items-center p-2 text-[12px] text-black rounded cursor-pointer ">
-                                                        <img className="w-5 h-5 rounded-full mr-[10px]" src="../src/assets/profile.png" alt="" />
+                                                        <img className="w-5 h-5 rounded-full mr-[10px]" src="./assets/profile.png" alt="" />
                                                         1AZFAFASFASZ
                                                     </span>
                                                 </div>
@@ -142,8 +128,8 @@ export default function App() {
                                                         <img className="h-[220px] w-[220px] rounded-[15px]" src={"./songs_images/" + item.img_src} alt="" />
                                                         <button className="h-[220px] w-[220px] rounded-[15px] absolute top-0 bg-[#000000a6]" onClick={() => played(item.title, item.artist, item.src)}>
                                                             {current_song == item.title && isPlaying ?
-                                                                <img className="h-[80px] w-[80px] m-auto" src="../src/assets/pause.png" alt="" /> :
-                                                                <img className="h-[80px] w-[80px] m-auto" src="../src/assets/play.png" alt="" />}
+                                                                <img className="h-[80px] w-[80px] m-auto" src="./assets/pause.png" alt="" /> :
+                                                                <img className="h-[80px] w-[80px] m-auto" src="./assets/play.png" alt="" />}
 
                                                         </button>
                                                     </div>
@@ -161,7 +147,7 @@ export default function App() {
                                                 <div class="grid grid-cols-2">
                                                     <div className="items-start"> <span className="inline-block font-semibold">STARTING BID</span> </div>
                                                     <div className="items-end text-right">
-                                                        <img className="h-[20px] w-[20px] rounded-[15px] inline-block" src="../src/assets/crypto_icon.png" alt="" />
+                                                        <img className="h-[20px] w-[20px] rounded-[15px] inline-block" src="./assets/crypto_icon.png" alt="" />
                                                         <span className="inline-block font-medium mr-[10px] ml-[5px]">1</span>
                                                         <span className="inline-block font-normal">$ 43.42</span>
                                                     </div>
@@ -183,7 +169,7 @@ export default function App() {
                 className="absolute z-10 w-auto min-w-full min-h-full max-w-none"
             >
                 <source
-                    src="./src/assets/bg_vid.mp4"
+                    src="./bg_vid.mp4"
                     type="video/mp4"
                 />
                 Your browser does not support the video tag.
